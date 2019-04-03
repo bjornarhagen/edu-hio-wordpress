11d076ca5b81e8f027816e5cfb46434d8a159ee7
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-03 16:26:32 +0200

    Remove unwanted content

--- wp-content/themes/havnehotellet-i-halden/template-parts/content.php
+++ wp-content/themes/havnehotellet-i-halden/template-parts/content.php
@@ -18,42 +18,34 @@
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
-		if ( 'post' === get_post_type() ) :
+		
 			?>
-			<div class="entry-meta">
-				<?php
-				havnehotellet_i_halden_posted_on();
-				havnehotellet_i_halden_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php havnehotellet_i_halden_post_thumbnail(); ?>
+	<section class="entry-content-wrapper">
+		<?php havnehotellet_i_halden_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'havnehotellet-i-halden' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+		<div class="entry-content">
+			<?php
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'havnehotellet-i-halden' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'havnehotellet-i-halden' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php havnehotellet_i_halden_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'havnehotellet-i-halden' ),
+				'after'  => '</div>',
+			) );
+			?>
+		</div><!-- .entry-content -->
+		<a href="#">Les mer</a>
+	</section>
 </article><!-- #post-<?php the_ID(); ?> -->
