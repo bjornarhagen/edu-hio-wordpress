3832803c92ddddeb02a1fca6f0760bcb7995ea4e
bereketgadhanom <bereketgadhanom@gmail.com>
2019-04-04 16:47:03 +0200

    Kontakt oss og restaurant  templates ble lagt

---
+++ wp-content/themes/havnehotellet-i-halden/template-parts/page-retaurant.php
@@ -0,0 +1,18 @@
+<?php /* Template Name: restaurant 
+    * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+    *
+    * @package Havnehotellet_i_Halden
+*/ ?>
+ 
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <?php
+            the_content();
+        ?>
+    </main>
+
+</div>
+  
+<?php get_footer(); ?>
