d6e86551d28027460cc4781867c76d3d1536fd8a
bereketgadhanom <bereketgadhanom@gmail.com>
2019-04-04 19:02:17 +0200

    Sletter div og lagt til kommentarer
    Sletter div og lagt til kommentarer til kontakt oss og restaurant templeter

--- wp-content/themes/havnehotellet-i-halden/template-parts/page-kontakt-oss.php
+++ wp-content/themes/havnehotellet-i-halden/template-parts/page-kontakt-oss.php
@@ -1,24 +1,26 @@
-<?php /* Template Name: kontakt oss 
-    * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
-    *
-    * @package Havnehotellet_i_Halden
-*/ ?>
- 
+<?php 
+    /* Template Name: kontakt oss 
+        * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+        *
+        * @package Havnehotellet_i_Halden
+    */ 
+?>
+  <!-- Hente header -->
 <?php get_header(); ?>
  
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-     
-    <?php
-        the_content();
-    ?>    
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1113.3045993390604!2d11.374066338380414!3d59.12043012889054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464412e79ad06d7f%3A0xbb42eec55d72276d!2sMathias+Bj%C3%B8rns+gate%2C+Halden!5e0!3m2!1sno!2sno!4v1554376651339!5m2!1sno!2sno" 
-        width="400" height="250" frameborder="0" style="border:0" 
-        allowfullscreen>
-    </iframe>
+    <main id="main-kontakt-oss" class="site-main" role="main">     
+        
+        <!-- Hente innholdet -->
+        <?php the_content(); ?>  
+        
+        <!-- Kart -->
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1113.3045993390604!2d11.374066338380414!3d59.12043012889054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464412e79ad06d7f%3A0xbb42eec55d72276d!2sMathias+Bj%C3%B8rns+gate%2C+Halden!5e0!3m2!1sno!2sno!4v1554376651339!5m2!1sno!2sno" 
+            width="400" height="250" frameborder="0" style="border:0" 
+            allowfullscreen>
+        </iframe>
+
     </main>
-</div>
-  
+  <!-- Hente foot -->
 <?php get_footer(); ?>
 
 
--- wp-content/themes/havnehotellet-i-halden/template-parts/page-retaurant.php
+++ wp-content/themes/havnehotellet-i-halden/template-parts/page-retaurant.php
@@ -1,18 +1,19 @@
-<?php /* Template Name: restaurant 
+<?php 
+    /* Template Name: restaurant 
     * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
     *
     * @package Havnehotellet_i_Halden
-*/ ?>
- 
+    */ 
+?>
+  <!-- Hente header -->
 <?php get_header(); ?>
+    
+    <main id="main-restaurant" class="site-main" role="main">
+       
+        <!-- Henter innholdet  -->
+        <?php the_content(); ?>
 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-        <?php
-            the_content();
-        ?>
     </main>
-
 </div>
-  
+  <!-- Henter footer -->
 <?php get_footer(); ?>
