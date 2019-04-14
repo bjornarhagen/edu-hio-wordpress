8b5069705cc7259668da6096445f6b6ab51d4aca
bereketgadhanom <bereketgadhanom@gmail.com>
2019-04-15 01:57:10 +0200

    Oppdatert kontakt-oss og restaurant sidene

--- wp-content/themes/havnehotellet-i-halden/template-parts/page-kontakt-oss.php
+++ wp-content/themes/havnehotellet-i-halden/template-parts/page-kontakt-oss.php
@@ -5,22 +5,24 @@
         * @package Havnehotellet_i_Halden
     */ 
 ?>
-  <!-- Hente header -->
-<?php get_header(); ?>
- 
-    <main id="main-kontakt-oss" class="site-main" role="main">     
-        
-        <!-- Hente innholdet -->
-        <?php the_content(); ?>  
-        
-        <!-- Kart -->
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1113.3045993390604!2d11.374066338380414!3d59.12043012889054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464412e79ad06d7f%3A0xbb42eec55d72276d!2sMathias+Bj%C3%B8rns+gate%2C+Halden!5e0!3m2!1sno!2sno!4v1554376651339!5m2!1sno!2sno" 
-            width="400" height="250" frameborder="0" style="border:0" 
-            allowfullscreen>
-        </iframe>
-
+<?php get_header();  ?>
+    <main id="kontakt-oss-main" class="site-main" role="main"> 
+        <?php havnehotellet_i_halden_post_thumbnail(); ?>
+        <?php
+            if(have_posts()):   
+                while(have_posts()): the_post();?> 
+                    <?php the_title(); ?> 
+                    <?php the_content(); ?>
+        <?php   endwhile;
+            endif;
+        ?> 
+        <div id="kart">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1113.3045993390604!2d11.374066338380414!3d59.12043012889054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464412e79ad06d7f%3A0xbb42eec55d72276d!2sMathias+Bj%C3%B8rns+gate%2C+Halden!5e0!3m2!1sno!2sno!4v1554376651339!5m2!1sno!2sno" 
+                height="250" width="99%"
+                allowfullscreen>
+            </iframe>
+        </div>
     </main>
-  <!-- Hente foot -->
 <?php get_footer(); ?>
 
 
---
+++ wp-content/themes/havnehotellet-i-halden/template-parts/page-restaurant.php
@@ -0,0 +1,15 @@
+<?php 
+    /* Template Name: restaurant 
+    * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+    *
+    * @package Havnehotellet_i_Halden
+    */ 
+?>
+ <?php get_header(); ?>
+
+    <main id="main-restaurant" class="site-main" role="main">  
+      <?php the_title(); ?>
+      <?php the_content(); ?>
+    </main>
+
+<?php get_footer(); ?>
