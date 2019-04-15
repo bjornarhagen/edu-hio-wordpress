58be4c5b625e345caaac9801d736fe8e12b7a3d0
bereketgadhanom <bereketgadhanom@gmail.com>
2019-04-15 23:02:59 +0200

    Lagt skjema og permalink til restaurant siden

--- wp-content/themes/havnehotellet-i-halden/template-parts/page-restaurant.php
+++ wp-content/themes/havnehotellet-i-halden/template-parts/page-restaurant.php
@@ -6,10 +6,27 @@
     */ 
 ?>
  <?php get_header(); ?>
+    <main id="restaurant-main">
+        <?php   
+          the_title(); 
+          the_content();
+          
+          $posts = get_posts();
+            foreach ( $posts as $post ) : ?>  
+            <a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a>
+        <?php endforeach;?>
 
-    <main id="main-restaurant" class="site-main" role="main">  
-      <?php the_title(); ?>
-      <?php the_content(); ?>
-    </main>
-
+          <form action="" id="bestill-form" method="post">
+              <h2>Reserver et bord </h2>
+              <label for="kort_id" >&#128288;</label>
+              <input type="text" name="kort_id"  placeholder="Kort id">
+              <label for="date" >&#128467;</label>
+              <input type="date" name="text" placeholder="Dato"> 
+              <label for="personer" >&#128694;</label>     
+              <input type="text" name="personer" placeholder="2 Personer">
+              <label for="time" >&#128343;</label>
+              <input type="time" name="time" placeholder="Time">
+              <button type="submit">Bestill</button>
+          </form>
+    </main>      
 <?php get_footer(); ?>
