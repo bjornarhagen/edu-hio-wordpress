45d6f96fe598534152e4352e46e1949a7e387a32
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-04-24 15:20:47 +0200

    Oppdater header til å funke med flere side typer: archive, search, 404

--- wp-content/themes/havnehotellet-i-halden/header.php
+++ wp-content/themes/havnehotellet-i-halden/header.php
@@ -52,31 +52,31 @@
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
-		<?php
-			while ( have_posts() ) :
-				the_post();
+        <?php
 
-				$header_display = get_field('header_display');
+        // while (have_posts()):
+            // the_post();
 
-    // Get header fields if the header is set to display
-    if (!empty($header_display)):
-        $header_heading = get_field('header_heading');
-        $header_text = get_field('header_text');
-        $header_text_color = get_field('header_text_color');
-        $header_overlay_color = get_field('header_overlay_color');
-        $header_overlay_opacity = (int) get_field('header_overlay_opacity');
+            if (function_exists('get_field') && !empty(get_field('header_display'))):
+                // Get header fields if the header is set to display
+                $header_heading = get_field('header_heading');
+                $header_text = get_field('header_text');
+                $header_text_color = get_field('header_text_color');
+                $header_overlay_color = get_field('header_overlay_color');
+                $header_overlay_opacity = (int) get_field('header_overlay_opacity');
+                $header_image_alignment = get_field('header_image_alignment');
 
-        // Set fallback text color
-        if (empty($header_text_color)) {
-            $header_text_color = '#000';
-        }
+                // Set fallback text color
+                if (empty($header_text_color)) {
+                    $header_text_color = '#fff';
+                }
 
-        // Set fallback overlay color
-        if (empty($header_overlay_color)) {
-            $header_overlay_color = '#fff';
-        }
+                // Set fallback overlay color
+                if (empty($header_overlay_color)) {
+                    $header_overlay_color = '#000';
+                }
 
-        // convert opacity from range 0-100 to 0-1
+                // convert opacity from range 0-100 to 0-1
         $header_overlay_opacity = $header_overlay_opacity / 100;
 
         // Get regular page title as fallback
@@ -91,25 +91,50 @@
         if (has_post_thumbnail(get_the_ID())):
             $thumbnail_id = get_post_thumbnail_id(get_the_ID());
             $header_image = wp_get_attachment_image_src($thumbnail_id, 'single-post-thumbnail');
-            $header_image = $header_image[0];
-        endif;
+                    $header_image = $header_image[0];
+                endif;
 
-        // Make the variables available to the template part
-        set_query_var('header_heading', $header_heading);
-        set_query_var('header_text', $header_text);
-        set_query_var('header_image', $header_image);
-        set_query_var('header_text_color', $header_text_color);
-        set_query_var('header_overlay_color', $header_overlay_color);
-        set_query_var('header_overlay_opacity', $header_overlay_opacity);
+                // Dynamic styling for header template part
+                ?>
+                <style>
+                    .page-header .page-header-overlay {
+                        opacity: <?=$header_overlay_opacity?>;
+                        background-color: <?=$header_overlay_color?>;
+                    }
 
-        // Get header template part
-        if (is_front_page()):
-            get_template_part('template-parts/header', 'front_page');
-        else:
-            get_template_part('template-parts/header', get_post_type());
-        endif;
-    else:
-        get_template_part('template-parts/header', 'none');
-    endif;
-endwhile; // End of the loop.
-?>
+                    .page-header .page-header-background {
+                        background-image: url('<?=$header_image?>');
+                        background-position: center <?=$header_image_alignment?>;
+                    }
+
+                    .page-header .page-header-content {
+                        color: <?=$header_text_color?>;
+                    }
+                </style>
+                <?php
+
+                // Make the variables available to the template part
+                set_query_var('header_heading', $header_heading);
+                set_query_var('header_text', $header_text);
+
+                // Get header template part
+                if (is_front_page()):
+                    // Get special front-page header fields
+                    set_query_var('booking_info_text', get_field('booking_info_text'));
+                    set_query_var('booking_info_link', get_field('booking_info_link'));
+
+                    get_template_part('template-parts/header', 'front_page');
+                else:
+                    get_template_part('template-parts/header', get_post_type());
+                endif;
+            elseif (is_search()):
+                get_template_part('template-parts/header', 'search');
+            elseif (is_archive()):
+                get_template_part('template-parts/header', 'archive');
+            elseif (is_404()):
+                get_template_part('template-parts/header', '404');
+            else:
+                get_template_part('template-parts/header', 'none');
+            endif;
+        // endwhile;
+    ?>
--- wp-content/themes/havnehotellet-i-halden/template-parts/header-archive.php
+++ wp-content/themes/havnehotellet-i-halden/template-parts/header-archive.php
@@ -1,3 +1,14 @@
+<?php
+  // Sjekk om siden er custom post type
+  if ( is_post_type_archive() ) {
+    $header_heading = post_type_archive_title();
+  } else {
+    $header_heading =  get_the_archive_title( );
+  }
+
+  $header_text = get_the_archive_description();
+?>
+
 <header class="page-header">
   <div id="page-navigation-padding"></div>
   <div class="page-header-overlay"></div>
