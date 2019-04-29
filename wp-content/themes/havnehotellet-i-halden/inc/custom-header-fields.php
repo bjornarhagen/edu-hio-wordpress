be9529ec4fbe9e8a8941799a86f9857d1a871709
bereketgadhanom <bereketgadhanom@gmail.com>
2019-04-29 22:34:57 +0200

    Fjernet gamle kode

--- wp-content/themes/havnehotellet-i-halden/inc/custom-header-fields.php
+++ wp-content/themes/havnehotellet-i-halden/inc/custom-header-fields.php
@@ -1,114 +1,10 @@
 <?php
- 
-if( function_exists('acf_add_local_field_group') ):
-
-acf_add_local_field_group(array(
-	'key' => 'group_5ca500fdc4cea',
-	'title' => 'Header',
-	'fields' => array(
-		array(
-			'key' => 'field_5ca64fc7aaf97',
-			'label' => 'Vis / skjul',
-			'name' => 'header_display',
-			'type' => 'true_false',
-			'instructions' => '',
-			'required' => 0,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'message' => '',
-			'default_value' => 1,
-			'ui' => 1,
-			'ui_on_text' => 'Vis',
-			'ui_off_text' => 'Skjul',
-		),
-		array(
-			'key' => 'field_5ca50114c9a13',
-			'label' => 'Header - overskrift',
-			'name' => 'header_heading',
-			'type' => 'text',
-			'instructions' => '',
-			'required' => 0,
-			'conditional_logic' => array(
-				array(
-					array(
-						'field' => 'field_5ca64fc7aaf97',
-						'operator' => '==',
-						'value' => '1',
-					),
-				),
-			),
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'default_value' => '',
-			'placeholder' => '',
-			'prepend' => '',
-			'append' => '',
-			'maxlength' => '',
-		),
-		array(
-			'key' => 'field_5ca50120c9a14',
-			'label' => 'Header - tekst',
-			'name' => 'header_text',
-			'type' => 'textarea',
-			'instructions' => '',
-			'required' => 0,
-			'conditional_logic' => array(
-				array(
-					array(
-						'field' => 'field_5ca64fc7aaf97',
-						'operator' => '==',
-						'value' => '1',
-					),
-				),
-			),
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'default_value' => '',
-			'placeholder' => '',
-			'maxlength' => '',
-			'rows' => 4,
-			'new_lines' => '',
-		),
-	),
-	'location' => array(
-		array(
-			array(
-				'param' => 'post_type',
-				'operator' => '==',
-				'value' => 'page',
-			),
-		),
-	),
-	'menu_order' => 0,
-	'position' => 'acf_after_title',
-	'style' => 'default',
-	'label_placement' => 'top',
-	'instruction_placement' => 'label',
-	'hide_on_screen' => '',
-	'active' => true,
-	'description' => '',
-));
-
-
-
 if (function_exists('acf_add_local_field_group')):
     $acf_files = ['custom-header-fields.json', 'custom-header-fields-frontpage-extra.json'];
-
     foreach ($acf_files as $acf_file) {
         $acf_json = file_get_contents(__DIR__ . '/acf/' . $acf_file);
         $acf_json = $acf_json;
         $acf_fields = json_decode($acf_json, true);
-
         // Did the JSON decode finish without any errors?
         if (json_last_error() === JSON_ERROR_NONE && is_array($acf_fields)) {
             // ACF's JSON export wraps all fields inside an array
@@ -118,5 +14,4 @@ if (function_exists('acf_add_local_field_group')):
             throw new Exception("Couldn't import ACF fields", 500);
         }
     }
-endif;
 endif;
--- wp-content/themes/havnehotellet-i-halden/inc/custom-header.php
+++ wp-content/themes/havnehotellet-i-halden/inc/custom-header.php
@@ -7,82 +7,28 @@ if (function_exists('get_field') && !empty(get_field('header_display'))) :
   $header_overlay_color = get_field('header_overlay_color');
   $header_overlay_opacity = (int)get_field('header_overlay_opacity');
   $header_image_alignment = get_field('header_image_alignment');
-
-
-/**
- * Set up the WordPress core custom header feature.
- *
- * @uses havnehotellet_i_halden_header_style()
- */
-function havnehotellet_i_halden_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( 'havnehotellet_i_halden_custom_header_args', array(
-		'default-image'          => get_stylesheet_directory_uri() . '/images/havnehotellet-i-halden.jpg',
-		'default-text-color'     => '000000',
-		'width'                  => 1920,
-		'height'                 => 600,
-		'flex-height'            => true,
-		'wp-head-callback'       => 'havnehotellet_i_halden_header_style',
-	) ) );
-
-	require(__DIR__ . '/custom-header-fields.php');
-}
-add_action( 'after_setup_theme', 'havnehotellet_i_halden_custom_header_setup' );
-  
-// Set fallback text color
+  // Set fallback text color
   if (empty($header_text_color)) {
     $header_text_color = '#fff';
   }
-
   // Set fallback overlay color
   if (empty($header_overlay_color)) {
     $header_overlay_color = '#000';
   }
-
-
-		/*
-		* If no custom options for text are set, let's bail.
-		* get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
-		*/
-		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
-			$header_text_color = false;
-		}
-
   // convert opacity from range 0-100 to 0-1
   $header_overlay_opacity = $header_overlay_opacity / 100;
-
-
   // Get regular page title as fallback
   if (empty($header_heading) && is_singular()) :
     $header_heading = get_the_title();
   endif;
-
-		// If we get this far, we have custom styles. Let's do this.
-		?>
-		<style type="text/css">
-			.page-header {
-				background-image: url(<?= esc_attr( $header_image ); ?>);
-			}
-
-			<?php if ( $header_text_color !== false ) : ?>
-			.page-title,
-			.page-description {
-				color: #<?= esc_attr( $header_text_color ); ?>;
-			}
-			<?php endif; ?>
-		</style>
-		<?php
-	
-
   // Get fallback background image
   $header_image = get_stylesheet_directory_uri() . '/images/havnehotellet-i-halden.jpg';
-
   // Get this posts featured image
   if (has_post_thumbnail(get_the_ID())) :
     $thumbnail_id = get_post_thumbnail_id(get_the_ID());
     $header_image = wp_get_attachment_image_src($thumbnail_id, 'single-post-thumbnail');
     $header_image = $header_image[0];
   endif;
-
   // Dynamic styling for header template part
   ?>
   <style>
@@ -90,28 +36,23 @@ add_action( 'after_setup_theme', 'havnehotellet_i_halden_custom_header_setup' );
       opacity: <?= $header_overlay_opacity ?>;
       background-color: <?= $header_overlay_color ?>;
     }
-
     .page-header .page-header-background {
       background-image: url('<?= $header_image ?>');
       background-position: center <?= $header_image_alignment ?>;
     }
-
     .page-header .page-header-content {
       color: <?= $header_text_color ?>;
     }
   </style>
   <?php
-
   // Make the variables available to the template part
   set_query_var('header_heading', $header_heading);
   set_query_var('header_text', $header_text);
-
   // Get header template part
   if (is_front_page()) :
     // Get special front-page header fields
     set_query_var('booking_info_text', get_field('booking_info_text'));
     set_query_var('booking_info_link', get_field('booking_info_link'));
-
     get_template_part('template-parts/header', 'front_page');
   else :
     get_template_part('template-parts/header', get_post_type());
@@ -124,9 +65,4 @@ elseif (is_404()) :
   get_template_part('template-parts/header', '404');
 else :
   get_template_part('template-parts/header', 'none');
-
 endif;
-
-
-
-
