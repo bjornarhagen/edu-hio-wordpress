0545bc229663e32b1f5237f9aebc488d723dd0c6
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-05-02 12:42:51 +0200

    Formatering

--- wp-content/themes/havnehotellet-i-halden/inc/custom-header-fields.php
+++ wp-content/themes/havnehotellet-i-halden/inc/custom-header-fields.php
@@ -1,10 +1,13 @@
 <?php
+
 if (function_exists('acf_add_local_field_group')):
     $acf_files = ['custom-header-fields.json', 'custom-header-fields-frontpage-extra.json'];
+
     foreach ($acf_files as $acf_file) {
         $acf_json = file_get_contents(__DIR__ . '/acf/' . $acf_file);
         $acf_json = $acf_json;
         $acf_fields = json_decode($acf_json, true);
+
         // Did the JSON decode finish without any errors?
         if (json_last_error() === JSON_ERROR_NONE && is_array($acf_fields)) {
             // ACF's JSON export wraps all fields inside an array
--- wp-content/themes/havnehotellet-i-halden/inc/custom-header.php
+++ wp-content/themes/havnehotellet-i-halden/inc/custom-header.php
@@ -7,28 +7,35 @@ if (function_exists('get_field') && !empty(get_field('header_display'))) :
   $header_overlay_color = get_field('header_overlay_color');
   $header_overlay_opacity = (int)get_field('header_overlay_opacity');
   $header_image_alignment = get_field('header_image_alignment');
+
   // Set fallback text color
   if (empty($header_text_color)) {
     $header_text_color = '#fff';
   }
+
   // Set fallback overlay color
   if (empty($header_overlay_color)) {
     $header_overlay_color = '#000';
   }
+
   // convert opacity from range 0-100 to 0-1
   $header_overlay_opacity = $header_overlay_opacity / 100;
+
   // Get regular page title as fallback
   if (empty($header_heading) && is_singular()) :
     $header_heading = get_the_title();
   endif;
+
   // Get fallback background image
   $header_image = get_stylesheet_directory_uri() . '/images/havnehotellet-i-halden.jpg';
+
   // Get this posts featured image
   if (has_post_thumbnail(get_the_ID())) :
     $thumbnail_id = get_post_thumbnail_id(get_the_ID());
     $header_image = wp_get_attachment_image_src($thumbnail_id, 'single-post-thumbnail');
     $header_image = $header_image[0];
   endif;
+
   // Dynamic styling for header template part
   ?>
   <style>
@@ -36,18 +43,22 @@ if (function_exists('get_field') && !empty(get_field('header_display'))) :
       opacity: <?= $header_overlay_opacity ?>;
       background-color: <?= $header_overlay_color ?>;
     }
+
     .page-header .page-header-background {
       background-image: url('<?= $header_image ?>');
       background-position: center <?= $header_image_alignment ?>;
     }
+
     .page-header .page-header-content {
       color: <?= $header_text_color ?>;
     }
   </style>
   <?php
+
   // Make the variables available to the template part
   set_query_var('header_heading', $header_heading);
   set_query_var('header_text', $header_text);
+
   // Get header template part
   if (is_front_page()) :
     // Get special front-page header fields
